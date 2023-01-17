41e71a4f5d5cb03ad3a36b2abe1182326c096778
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-01-17 14:00:10 +0100

    FlashLoan initial commit

---
+++ contracts/FlashLoan.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract FlashLoan {
+    
+}
+ 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "dotenv";
+import "dotenv/config";
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PIVATE_KEY = process.env.PIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.10",
   networks: {
     goerli: {
       url: GOERLI_RPC_URL,
