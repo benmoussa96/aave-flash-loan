b13e4e7fc52087e6a2dff456d6ad75d66cfbda63
ghaieth96 <ghaiethbmoussa@gmail.com>
2023-01-17 14:47:38 +0100

    FlashLoan Contract

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -8,11 +8,18 @@ import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/bas
 import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
+error FundMe__NotOwner();
+
 contract FlashLoan is FlashLoanSimpleReceiverBase {
-    address payable owner;
+    address private immutable i_owner;
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) { revert FundMe__NotOwner(); }
+        _;
+    }
 
     constructor(address _addressProvider) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
-        owner = payable(msg.sender);
+        i_owner = msg.sender;
     }
 
     function executeOperation(
@@ -22,8 +29,31 @@ contract FlashLoan is FlashLoanSimpleReceiverBase {
     address initiator,
     bytes calldata params
   ) external override returns (bool) {
+    uint256 amountOwed = amount + premium;
+    IERC20(asset).approve(address(POOL), amountOwed);
 
+    return true; 
   }
-    
+
+  function requestFlashLoan(address _token, uint256 _amount) public {
+    address receiverAddress = address(this);
+    address asset = _token;
+    uint256 amount = _amount;
+    bytes memory params= "";
+    uint16 referralCode = 0;
+
+    POOL.flashLoanSimple(receiverAddress, asset, amount, params, referralCode);
+  }
+
+  function getBalance(address _tokenAddress) external view returns (uint256) {
+    return IERC20(_tokenAddress).balanceOf(address(this));
+  }
+
+  function withdraw(address _tokenAddress) external onlyOwner {
+    IERC20 token = IERC20(_tokenAddress);
+    token.transfer(payable(msg.sender), token.balanceOf(address(this)));
+  }
+
+  receive() external payable {}
 }
  
